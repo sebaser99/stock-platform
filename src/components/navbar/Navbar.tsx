0a8e74912fffa818d38cfe0c859741952c9a0afd
Sebastian Rodriguez <sebaser99@gmail.com>
2024-12-09 18:31:48 -0500

    agregamos enlaces

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -136,10 +136,23 @@ export const Navbar = () => {
         }
         
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><Link to="/"  
+                      style={{
+                        fontWeight: location.pathname === "/" ? "bold" : "normal",
+                        textDecoration: "none",
+                      }}>Home</Link></li>
+           <li><Link to="/MyStocks"
+                      style={{
+                        fontWeight: location.pathname === "/MyStocks" ? "bold" : "normal",
+                        textDecoration: "none",
+                      }}
+                      >MyStocks</Link></li>
+                <li><Link to="/MyFunds"
+                      style={{
+                        fontWeight: location.pathname === "/MyFunds" ? "bold" : "normal",
+                        textDecoration: "none",
+                      }}
+                    >MyFunds</Link></li>
         </ul>
       </div>
     </>
